2ac101e24c3ada7f5a0758a45519aea9a018d831
zhanz-python <zhanzalayahya@gmail.com>
2024-04-03 12:20:41 -0400

    Zakariya's changes

--- main.py
+++ main.py
@@ -1,88 +1,69 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import pyodbc
+import csv
+from io import StringIO
 import os
-import pyodbc, struct
-from azure import identity
-
-from fastapi import FastAPI
 import pandas as pd
-from datetime import datetime
-from fastapi.responses import FileResponse
 
 app = FastAPI()
+
+driver = '{ODBC Driver 17 for SQL Server}'
+server = 'server-sampledb.database.windows.net'
+database = 'sample-db'
+username = 'admin123'
+password = 'admin!123'
+connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-#Need parameters for database name, server
-AZURE_SQL_CONNECTIONSTRING=''
-
-@app.get("/tables/{server_name}/{database_name}")
-def get_tables(server_name: str, database_name: str):
-    output = ""
-    column = []
-    AZURE_SQL_CONNECTIONSTRING = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:%s.database.windows.net,1433;Database=%s;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30" % (server_name, database_name)
-    with get_conn(AZURE_SQL_CONNECTIONSTRING) as conn:
+@app.get("/azure-connection")
+def azure_connection():
+    try:
+        conn = pyodbc.connect(connection_string)
         cursor = conn.cursor()
-        cursor.execute("select schema_name(t.schema_id) as schema_name, t.name as table_name, t.create_date, t.modify_date from sys.tables t order by schema_name, table_name;")
 
-        
-        results = (cursor.fetchall())
-        for tupler in cursor.description:
-            #get names of columns (first value in tuple)
-            column.append(tupler[0])
-            
-        results.insert(0, column)
-        create_csv("table_info", results)
+        cursor.execute("SELECT @@version;")
+        row = cursor.fetchone()
+        return {"Message": "Connection successful", "Version": row[0]}
 
-        #find file path and dynamically change string
-        output = "File has been saved to Downloads "
+    except pyodbc.Error as error:
+        return {"message": f"Error connecting to Azure SQL Database: {error}"}
 
+    finally:
+        if conn:
+            conn.close()
 
-    return output
+@app.get("/import-iris")
+def import_iris():
+    try:
+        file_name = 'iris.csv'
+        table_name = os.path.splitext(file_name)[0]
 
-@app.get("/schemas/{server_name}/{database_name}")
-def get_tables(server_name: str, database_name: str):
-    output = ""
-    
-    AZURE_SQL_CONNECTIONSTRING = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:%s.database.windows.net,1433;Database=%s;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30" % (server_name, database_name)
-    with get_conn(AZURE_SQL_CONNECTIONSTRING) as conn:
+        df = pd.read_csv(file_name)
+
+        conn = pyodbc.connect(connection_string)
         cursor = conn.cursor()
-        cursor.execute("select schema_name(t.schema_id) as schema_name, t.name as table_name from sys.tables t  order by schema_name, table_name;")
 
-        results = (cursor.fetchall())
-        for table in results:
-            if table != "table_name":
-                print(type(table))
-                table_schema = table[0]
-                table_name = table[1]
-                #select everything from each table, make into csv
-                print(f"select * from {table_schema}.{table_name};")             
-                cursor.execute(f"select * from {table_schema}.{table_name};")
-                table_result = (cursor.fetchall())
+        # Create table
+        create_table_query = f"CREATE TABLE {table_name} ("
+        for column_name in df.columns:
+            column_name_cleaned = column_name.replace('.', '_')  # Replace dots with underscores
+            create_table_query += f"{column_name_cleaned} VARCHAR(255), "
+        create_table_query = create_table_query[:-2] + ");"
+        cursor.execute(create_table_query)
 
-                column = []
-                for tupler in cursor.description:
-                    #get names of columns (first value in tuple)
-                    column.append(tupler[0])
+        # Insert data
+        for index, row in df.iterrows():
+            placeholders = ",".join(["?"] * len(row))
+            columns = ",".join([col.replace('.', '_') for col in row.index])  # Replace dots with underscores
+            sql = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+            cursor.execute(sql, tuple(row))
+        conn.commit()
 
-                table_result.insert(0, column)
+        conn.close()
 
-                create_csv(f"{table_schema}_{table_name}", table_result)
-            
-        output = "File has been saved to Downloads "
-
-
-    return output
-
-def create_csv(filename: str, data):
-        df = pd.DataFrame(data)
-        df.to_csv(f'{filename}.csv', index=False)
-
-
-def get_conn(AZURE_SQL_CONNECTIONSTRING : str):
-    credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
-    token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
-    token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
-    SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
-    conn = pyodbc.connect(AZURE_SQL_CONNECTIONSTRING, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
-    return conn
+        return {"message": "Data imported successfully."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
